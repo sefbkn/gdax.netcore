29daa56ca946664ee63a5de2f56e49f9bdb9f472
UniversalAE <engineering@alligatortech.com>
2017-11-20 12:40:45 -0800

    Added support for history candles and post_only orders
    Added Candles.cs to types folder
    Modified OrderClient.cs with PlaceOrderAsync overload for post_only orders
    Modified ProductClient.cs to add GetHistoricRatesAsync method.

--- src/Api/Clients/OrderClient.cs
+++ src/Api/Clients/OrderClient.cs
@@ -12,24 +12,43 @@ namespace Boukenken.Gdax
         Task<ApiResponse<IEnumerable<Order>>> GetOpenOrdersAsync();
         Task<ApiResponse<IEnumerable<Guid>>> CancelOpenOrdersAsync(string productId = null);
         Task<ApiResponse<Order>> PlaceOrderAsync(string side, string productId, decimal size, decimal price, string type, string cancelAfter = null, string timeInForce = null);
+        Task<ApiResponse<Order>> PlaceOrderAsync(string side, string productId, decimal size, decimal price, string type, bool postOnly, string cancelAfter = null, string timeInForce = null);
     }
 
-    public class OrderClient : GdaxClient
+	public class OrderClient : GdaxClient
     {
         public OrderClient(string baseUrl, RequestAuthenticator authenticator)
             : base(baseUrl, authenticator)
         {
         }
 
-        public async Task<ApiResponse<Order>> PlaceOrderAsync(string side, string productId, decimal size, decimal price, string type, string cancelAfter = null, string timeInForce = null)
+		public async Task<ApiResponse<Order>> PlaceOrderAsync(string side, string productId, decimal size, decimal price, string type, bool postOnly, string cancelAfter = null, string timeInForce = null)
+		{
+			return await this.GetResponseAsync<Order>(
+				new ApiRequest(HttpMethod.Post, "/orders", Serialize(new
+				{
+					size = size,
+					side = side,
+					type = type,
+					price = price,
+					product_id = productId,
+					post_only = postOnly,
+					cancel_after = cancelAfter,
+					time_in_force = timeInForce
+				}))
+			);
+		}
+
+		public async Task<ApiResponse<Order>> PlaceOrderAsync(string side, string productId, decimal size, decimal price, string type, string cancelAfter = null, string timeInForce = null)
         {
-            return await this.GetResponseAsync<Order>(
-                new ApiRequest(HttpMethod.Post, "/orders", Serialize(new {
-                    size = size,
-                    side = side,
-                    type = type,
-                    price = price,
-                    product_id = productId,
+			return await this.GetResponseAsync<Order>(
+				new ApiRequest(HttpMethod.Post, "/orders", Serialize(new {
+					size = size,
+					side = side,
+					type = type,
+					price = price,
+					product_id = productId,
+					post_only = false,
                     cancel_after = cancelAfter,
                     time_in_force = timeInForce
                 }))
@@ -39,11 +58,18 @@ namespace Boukenken.Gdax
         public async Task<ApiResponse<IEnumerable<Order>>> GetOpenOrdersAsync()
         {
             return await this.GetResponseAsync<IEnumerable<Order>>(
-                new ApiRequest(HttpMethod.Get, "/orders")
+                new ApiRequest(HttpMethod.Get, "/orders?status=all")
             );
         }
 
-        public async Task<ApiResponse<IEnumerable<Guid>>> CancelOpenOrdersAsync(string productId = null)
+		public async Task<ApiResponse<Order>> GetOpenOrdersAsync(string Id)
+		{
+			return await this.GetResponseAsync<Order>(
+				new ApiRequest(HttpMethod.Get, "/orders/" + Id)
+			);
+		}
+
+		public async Task<ApiResponse<IEnumerable<Guid>>> CancelOpenOrdersAsync(string productId = null)
         {
             return await this.GetResponseAsync<IEnumerable<Guid>>(
                 new ApiRequest(HttpMethod.Delete, "/orders" + (productId == null ? "" : $"?product_id={productId}"))
