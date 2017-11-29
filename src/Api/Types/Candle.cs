4991fc5750fb76f464e7407500cdcfbd6e6f262e
psiops <psiopssoftware@gmail.com>
2017-11-29 17:48:39 +0100

    Adding HistoricRate endpoint based on code by @UniversalAE, only with proper indenting.

---
+++ src/Api/Types/Candle.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Boukenken.Gdax
+{
+    public class Candle
+    {
+        public long time { get; set; }
+        public decimal low { get; set; }
+        public decimal high { get; set; }
+        public decimal open { get; set; }
+        public decimal close { get; set; }
+        public decimal volume { get; set; }
+    }
+}
